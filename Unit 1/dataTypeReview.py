73378b66df61e0fa6d923410578a8ef8639801e5
jellS215 <144711104+jellS215@users.noreply.github.com>
2025-08-22 15:09:20 +0000

    8/22

---
+++ Unit 1/dataTypeReview.py
@@ -0,0 +1,17 @@
+#Print the following data types:
+#string 
+print("Good Morning")
+#boolean 
+print(20 > 10)
+#integer  
+x = 20
+print(x)
+#float 
+x = 5.5
+print(x)
+
+#print a list that features two of each data type
+
+Pantry = ["bread", "bananas", "Ravioli"]
+print(Pantry)
+print("there is more bread than bananas")
