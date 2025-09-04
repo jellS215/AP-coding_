637e39a4b6ec8cd8616374adbf53cddefbbd6e92
jellS215 <144711104+jellS215@users.noreply.github.com>
2025-09-04 15:07:54 +0000

    9/4

---
+++ Unit 1/arrays.py
@@ -0,0 +1,14 @@
+numbers= [1,20,39,48,72,83]
+value = 48
+
+def numberThing(numbers, value):
+    for x in numbers:
+        if x==value:
+            break
+        
+        
+unorderList= [ 23, 600, 4, 91, 22, 49]
+unorderList.sort()
+print(unorderList)
+
+              
