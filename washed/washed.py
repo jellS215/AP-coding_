1b38bc4a65a64c765d0e21d9e372bbca4563acce
jellS215 <144711104+jellS215@users.noreply.github.com>
2025-12-05 16:06:54 +0000

    12/5

---
+++ washed/washed.py
@@ -0,0 +1,5 @@
+from helperfunctions import get_season_totals_by_position, get_player_stats, dataframe_to_png, plot_player_stat_by_week, plot_position_stat_bar, get_team_season_data, get_team_game_stats
+
+'washed means a player who is past their prime and no longer performs at a high level in their sport, falling below what people expect from them.'
+
+playerStats = plot_player_stat_by_week(2022, 'Jalen Hurts', 'passing_yards')
