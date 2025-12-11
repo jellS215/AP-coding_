f8b79600c0be66567dbe7fa50b264dac946c65a3
jellS215 <144711104+jellS215@users.noreply.github.com>
2025-12-11 15:47:57 +0000

    updating repo

---
+++ ratings.py/ratings.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import nfl_data_py as nfl 
+import matplotlib.pyplot as plt
+import numpy as np 
---
+++ ratings/ratings.py
@@ -0,0 +1,4 @@
+from helperFunctions import get_season_totals_by_position
+
+qb = get_season_totals_by_position(2023, "QB")
+print(qb)
