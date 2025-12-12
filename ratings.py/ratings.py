864d3c3d576dc8521bd2c5dbdbf282593a64583e
jellS215 <144711104+jellS215@users.noreply.github.com>
2025-12-12 15:53:43 +0000

    12/12

--- ratings.py/ratings.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import nfl_data_py as nfl 
-import matplotlib.pyplot as plt
-import numpy as np 
--- ratings/ratings.py
+++ ratings/ratings.py
@@ -1,4 +1,13 @@
-from helperFunctions import get_season_totals_by_position
+# from helperFunctions import get_season_totals_by_position
 
-qb = get_season_totals_by_position(2023, "QB")
-print(qb)
+# qb = get_season_totals_by_position('QB', 2023)
+
+# print(qb)
+
+'1. to be underrated is to be underestimated in value or ability in comparison to others, but in reality are much better than most.'
+'to be overrated is to be overestimated in value or ability in comparison to others, but in reality are much worse than most.'
+'3 s stats that i would say are a baseline for these ratings are yards per attempt, completion percentage, and touchdown to interception ratio.'
+
+'Players:'
+'Baker Mayfield is underrated, in 2024 he had a high completion percentage and a good touchdown to interception ratio, and a great yards gained from passing stat of 4500, but is often overlooked due to playing on a less successful team and being overshadowed by other QBs.'
+'CeeDee Lamb is overrated, in 2024 he had an alright total recieving yards, an alright yards per reception, and a low touchdown count, but is often praised for his athleticism and potential.'
